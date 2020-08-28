9fc1a839cd309c5a580315bd692e7604aa59148f
Aaron <Aaron@As-MacBook-Pro.local>
2020-08-28 09:25:59 -0700

    complete MinNumberOf Chairs

--- MinNumberOfChairs/Main.java
+++ MinNumberOfChairs/Main.java
@@ -2,11 +2,29 @@
 There are n guests who are invited to a party. The k-th guest will attend the party at time S[k] and leave the party at time E[k].
 Given an integer array S and an integer array E, both of length n, return an integer denoting the minimum number of chairs you need such that everyone attending the party can sit down.
 */
+import java.util.*;
 
 public class Main {
 
-    static int func(int[] s,int[] e){
-        return 0;
+    static int func(int[] arrival,int[] exit){
+        Arrays.sort(arrival);
+        Arrays.sort(exit);
+
+        int curr = 0, max = 0;
+        int i = 0, j = 0, len = arrival.length;
+        while(i < len && j < len) {
+            if(arrival[i] < exit[j]) {
+                i++;
+                curr++;
+                if (curr >= max) {
+                    max = curr;
+                }
+            } else {
+                curr--;
+                j++;                
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
@@ -17,10 +35,14 @@ public class Main {
         System.out.println(result);
         System.out.println(result==expected);
 
-        int[] s1 = {1, 2, 9, 5, 5};
-        int[] e1 = {4, 5, 12, 9, 12};
-        int expected = 3;
-        int result = func(s1, e1);
+        int[] s2 = {1, 2, 9, 5, 5};
+        int[] e2 = {4, 5, 12, 9, 12};
+        /*
+        1 2 5 5  9 
+        4 5 9 12 12 
+        */
+        expected = 2;
+        result = func(s2, e2);
         System.out.println(result);
         System.out.println(result==expected);
     }
