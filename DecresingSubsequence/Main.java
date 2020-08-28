10d1d8a006e4647dcfb7bd54062780ca3e9378cc
Aaron <Aaron@As-MacBook-Pro.local>
2020-08-28 10:01:57 -0700

    set up for decreasing subsequence

--- DecresingSubsequence/Main.java
+++ DecresingSubsequence/Main.java
@@ -6,6 +6,26 @@ Output the min number of subsequences you can get by splitting.
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hey");
+        int[] inputOne = {5, 2, 4, 3, 1, 6};
+        int expected = 3;
+        int result = func(inputOne);
+        System.out.println(result);
+        System.out.println(result == expected);
+
+        int[] inputTwo = {2, 9, 12, 13, 4, 7, 6, 5, 10};
+        expected = 4;
+        result = func(inputTwo);
+        System.out.println(result);
+        System.out.println(result == expected);
+
+        int[] inputThree = {1, 1, 1};
+        expected = 3;
+        result = func(inputThree);
+        System.out.println(result);
+        System.out.println(result == expected);
+    }
+
+    static int func(int[] arr){
+        return 0;
     }
 }
