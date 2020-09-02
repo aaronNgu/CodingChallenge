a9500ebbbf3193a94d2a39d1428697269f7f9f84
Aaron <Aaron@As-MacBook-Pro.local>
2020-09-02 12:17:31 -0700

    complete count substrings with exactly K distinct Chars

--- CountSubtringsWithExactlyKDistinctChars/Main.java
+++ CountSubtringsWithExactlyKDistinctChars/Main.java
@@ -5,6 +5,7 @@ return an int representing the number of substrings (not unique) of s with exact
 If the given string doesn't have k distinct characters, return 0.
 */
 
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,7 +19,26 @@ public class Main {
         System.out.println(result == expected);
     }
 
+    static int atMostK(String s, int k) {
+        int total = 0, left = 0, right = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (!map.containsKey(c)) map.put(c, 0);
+            map.put(c, 1);
+            while (map.size() > 2) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                total += right - left + 1;
+                left++;
+            }
+            total += right - left + 1;
+            right++;
+        }
+        return total;
+    }
+
     static int func(String s, int k) {
-        return 0;
+        return atMostK(s,k) - atMostK(s, k-1);
     }
 }
