616a3831cd11e955c50a199941d44a07df8c7623
Aaron <Aaron@As-MacBook-Pro.hitronhub.home>
2020-11-26 21:25:00 -0800

    Redo Min Cost to Connect All Nodes

--- MinCostToConnectAllNodes/Main.java
+++ MinCostToConnectAllNodes/Main.java
@@ -20,16 +20,87 @@ public class Main {
     }
 
     static int func(int n, int[][] edges, int[][] newEdges) {
-        Set<String> hset = new HashSet<String>();
+        PriorityQueue<int[]> q = new PriorityQueue<int[]>((a, b) -> a[2] - b[2]);
+        for (int[] edge: edges) q.add(new int[]{edge[0], edge[1], 0});
+        for (int[] newEdge: newEdges) q.add(newEdge);
+        int sum = 0;
+        int[] parents = new int[n+1];
+        Arrays.fill(parents, -1);
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int u = curr[0], v = curr[1], w = curr[2];
+            int up = find(u, parents), vp = find(v, parents);
+            if (up != vp) {
+                sum += w;
+                union(u, v, parents);
+            }
+        }
+        return sum;
+    }
+
+    static int find(int node,int[] parents) {
+        if (parents[node] < 0) return node;
+        int p = find(parents[node], parents);
+        parents[node] = p;
+        return p;
+    }
+
+    static void union(int u, int v, int[] parents) {
+        int up = find(u, parents);
+        int vp = find(v, parents);
+        parents[up] = vp;
+    }
+    /*
+    static int func(int n, int[][] edges, int[][] newEdges) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+
+        for (int[] edge: edges) {
+            q.add(new int[]{edge[0], edge[1], 0});
+        }
+
+        for (int[] newEdge: newEdges) {
+            q.add(newEdge);
+        }
+
+        int sum = 0;
+        int[] parents = new int[n+1];
+        Arrays.fill(parents, -1);
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int u = curr[0], v = curr[1], w = curr[2];
+            int up = find(u, parents), vp = find(v, parents);
+            if (up != vp) {
+                sum += w;
+                union(parents, u, v);
+            }
+        }
+        return sum;
+    }
+
+    static int find(int node, int[] parents) {
+        if (parents[node] < 0) return node;
+        int p = find(parents[node], parents);
+        parents[node] = p;
+        return p;
+    }
+
+    static void union(int[] parents, int u, int v) {
+        int up = find(u, parents);
+        int vp = find(v, parents);
+        parents[vp] = up;
+    }
+
+    static int func(int n, int[][] edges, int[][] newEdges) {
+        //Set<String> hset = new HashSet<String>();
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[2] - b[2]);
         for (int[] e : newEdges) {
-            String s = e[0] + " " +  e[1];
-            hset.add(s);
+            //String s = e[0] + " " +  e[1];
+            //hset.add(s);
             pq.add(e);
         }
         for (int[] e : edges) {
-            String s = e[0] + " " + e[1];
-            if (hset.contains(s)) continue;
+            // String s = e[0] + " " + e[1];
+            //if (hset.contains(s)) continue;
             int[] l = new int[]{e[0], e[1], 0};
             pq.add(l);
         }
@@ -61,4 +132,5 @@ public class Main {
         int vp = find(v, par);
         par[vp] = up;
     }
+    */
 }
